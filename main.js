418fd4327da73cb31013ce4563cf69ea9eae6b70
Musira <musiraallan@gmail.com>
2019-09-11 16:18:52 +0300

    reloading page - javascript

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // Musira Codes
+
+var playing = false;
+var score;
+
+// when the start/reset button is clicked
+document.getElementById("start_reset").onclick = function() {
+  // if playing
+  if (playing == true) {
+    location.reload(); // reload page
+  } else {
+    // if not playing
+    playing = true; // change mode to playing
+    score = 0; // set score to zero
+    document.getElementById("score_value").innerHTML = score;
+    // shoe countdown box
+    document.getElementById("time_remaining").style.display = "block";
+    // change button to reset
+    document.getElementById("start_reset").innerHTML = "Reset Game";
+  }
+};
